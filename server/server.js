9cfc03b08aed84683ff3deeaa47138d057e08872
Ato <zxcv123594@gmail.com>
2022-05-09 10:51:01 +0900

    Delete server directory

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const server = express()
-const server_port = 3001
-
-server.use(bodyParser.urlencoded({extended: true}));
-server.use(bodyParser.json());
-server.use(cookieParser());
