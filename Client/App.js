8d96ec1c2e8a40da2165331635aa18351c6d0aa9
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-05-11 15:40:32 +0900

    ClubScreen, JoinClubScreen, CreateClubScreen 추가 및 뒤로가기 버튼 추가

--- Client/App.js
+++ Client/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './Screen /MainScreen';
 import RegisterScreen from './Screen /RegisterScreen';
 import LoginScreen from './Screen /LoginScreen';
 import CameraScreen from './Screen /CameraScreen';
+import ClubScreen from './Screen /ClubScreen';
+import JoinClubScreen from './Screen /JoinClubScreen';
+import CreateClubScreen from './Screen /CreateClubScreen';
 const stack = createStackNavigator();
+
+function BackBtn() {
+  return (
+    <Image
+      source={require('./src/icon/back-btn.png')}
+      style={{ marginLeft: 10, width: 22, height: 22 }}
+    />
+  );
+}
 
 class App extends Component {
   render() {
@@ -36,7 +48,7 @@ class App extends Component {
                 fontSize: 20,
                 color: "#707070"
               },
-              headerBackTitleVisible: false, //headerBackTitleVisible을 이용해서 버튼 타이틀의 렌더링 여부를 동일하게 설정
+              headerBackTitleVisible: false,
               // headerBackImage: BackBtn,
             }}
           />
@@ -63,7 +75,41 @@ class App extends Component {
                 fontSize: 20,
                 color: "#707070"
               },
-              headerBackTitleVisible: false, //headerBackTitleVisible을 이용해서 버튼 타이틀의 렌더링 여부를 동일하게 설정
+              headerBackTitleVisible: false,
+            }}
+          />
+          <stack.Screen
+            name="Club"
+            component={ClubScreen}
+            options={{
+              title: "",
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: 20,
+                color: "#707070"
+              },
+              headerBackTitleVisible: false,
+              headerTransparent: true,
+            }}
+          />
+          <stack.Screen
+            name="JoinClub"
+            component={JoinClubScreen}
+            options={{
+              title: "",
+              headerBackTitleVisible: false,
+              headerTransparent: true,
+              headerBackImage: BackBtn,
+            }}
+          />
+          <stack.Screen
+            name="CreateClub"
+            component={CreateClubScreen}
+            options={{
+              title: "",
+              headerBackTitleVisible: false,
+              headerTransparent: true,
+              headerBackImage: BackBtn,
             }}
           />
         </stack.Navigator>
