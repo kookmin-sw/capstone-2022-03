a429e21aafda703dc1ac8521912ea01146764185
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-05-20 12:11:12 +0900

    Firebase 삭제 및 사진 저장 방식 변경

--- Client/Screen/CameraScreen.js
+++ Client/Screen/CameraScreen.js
@@ -5,8 +5,9 @@ import { useNavigation } from '@react-navigation/native';
 import ImagePicker from 'react-native-image-crop-picker';
 import Plus from '../src/icon/plus.png';
 import storage from '@react-native-firebase/storage';
-import config from '../config.json';
-import test from '../src/icon/test.jpeg';
+import CustomButton from '../src/CustomButton';
+import styles from '../src/Styles';
+
 
 export function CameraScreen() {
     const navigation = useNavigation();
@@ -17,13 +18,15 @@ export function CameraScreen() {
             width: 300,
             height: 400,
             cropping: true,
+            includeBase64: true,
         }).then(image => {
             console.log(image);
             captured_receipt({
                 uri: image.path,
                 width: image.width,
                 height: image.height,
-                mime: image.mime
+                mime: image.mime,
+                data: image.data,
             })
         }).catch(function (error) {
             console.log('There has been a problem with your fetch operation: ' + error.message);
@@ -36,92 +39,45 @@ export function CameraScreen() {
             console.log(receipt.uri);
             return <Image
                 source={{ uri: receipt.uri }}
-                style={styles.images}
+                style={extra.images}
             />
         } else {
-            return <View style={styles.images}>
+            return <View style={extra.images}>
                 <Image
                     source={Plus}
-                    style={styles.plusIcon}
+                    style={extra.plusIcon}
                 />
             </View>
         }
     }
 
-    async function postImage() {
-        if (receipt.uri === undefined) {
-            Alert.alert('영수증 사진을 찍어주세요.')
-        } else {
-            navigation.navigate('Loading');
-        }
-    }
-
-    const callGoogleVisionApi = async () => {
-        console.log("ocr분석시작");
-        console.log(config.googleCloud.api);
-        console.log(config.googleCloud.apiKey);
-        console.log(test.uri);
-        let googleVisionRes = await fetch(config.googleCloud.api + config.googleCloud.apiKey, {
-            method: 'POST',
-            body: JSON.stringify({
-                "requests": [
-                    {
-                        "features": [
-                            { "type": "DOCUMENT_TEXT_DETECTION" },
-                        ],
-                        "image": {
-                            "source": {
-                                "imageUri": "gs://kookmin-sw-capstone-2022-03.appspot.com/test2.jpeg"
-                            }
-                        }
-                    }
-                ]
-            })
-        });
-        await googleVisionRes.text()
-            .then(googleVisionRes => {
-                console.log(googleVisionRes)
-                if (googleVisionRes) {
-                    console.log('this.is response', googleVisionRes);
-                }
-            }).catch((error) => { console.log(error) })
-    }
-
-
-    const uploadImage = () => {
-        let ext = receipt.uri.split('/').pop();
-        console.log(ext)
-        const imgRef = storage().ref(ext);
-        let task = imgRef.putFile(receipt.uri);
-        task.then(() => {
-            console.log("Upload");
-        }).catch((e) => console.log(e));
-        // const filename = `${uuid()}.${ext}`;
-        // const imgRef = firebase.storage().ref(`receipt/${filename}`);
-    }
     return (
-        <View style={styles.cameraScreen}>
-            <View style={styles.TextView}>
-                <Text style={styles.text}>+를 터치하여 <Text style={styles.highlight}>영수증</Text>을 찍어주세요</Text>
+        <View style={[styles.container, { backgroundColor: styles.Color_Main2, alignItems: 'center', justifyContent: 'center' }]}>
+            <View style={{ marginTop: 150 }}></View>
+            <View style={extra.TextView}>
+                <Text style={extra.text}>+를 터치하여 <Text style={extra.highlight}>영수증</Text>을 찍어주세요</Text>
             </View>
             <TouchableOpacity onPress={runCamera} >
                 {renderImage(1)}
             </TouchableOpacity>
-            <Button onPress={uploadImage} title='제출하기'></Button>
-            <Button onPress={callGoogleVisionApi} title='ocr분석'></Button>
+            <View style={[{ marginTop: 50, width: '90%', height: 55, backgroundColor: 'theme' }]}>
+                <CustomButton
+                    buttonColor={'#4169e1'}
+                    title="등록"
+                    onPress={() => {
+                        navigation.navigate("ReceiptImage", {
+                            mime: receipt.mime,
+                            base64: receipt.data,
+                            path: receipt.uri,
+                        })
+                    }}
+                />
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    cameraScreen: {
-        display: 'flex',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f2f2f2',
-    },
-
+const extra = StyleSheet.create({
     images: {
         marginBottom: 100,
         width: 300,
@@ -132,29 +88,28 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: 'white'
     },
-
     plusIcon: {
         width: 50,
         height: 50,
     },
-
     TextView: {
         alignItems: "center",
         justifyContent: "center",
         marginBottom: 15,
     },
-
+    text: {
+        color: 'white',
+    },
     highlight: {
         width: 266,
         height: 19,
-        // fontFamily: "AppleSDGothicNeo",
         fontSize: 16,
         fontWeight: "500",
         fontStyle: "normal",
         letterSpacing: 0,
         textAlign: "center",
-        color: "#208cf7",
-    }
+        color: "white",
+    },
 })
 
 export default CameraScreen
